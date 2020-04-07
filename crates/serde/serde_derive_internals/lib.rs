dfad426b6fcdf87563325203e868d3bbb7facdd3
Mingshen Sun <bob@mssun.me>
2020-04-07 12:33:19 -0700

    Add serde 1.0.106 (#12)

---
+++ crates/serde/serde_derive_internals/lib.rs
@@ -0,0 +1,23 @@
+#![doc(html_root_url = "https://docs.rs/serde_derive_internals/0.25.0")]
+#![allow(unknown_lints, bare_trait_objects)]
+#![cfg_attr(feature = "cargo-clippy", allow(renamed_and_removed_lints))]
+#![cfg_attr(
+    feature = "cargo-clippy",
+    allow(
+        cognitive_complexity,
+        redundant_field_names,
+        trivially_copy_pass_by_ref,
+        wildcard_in_or_patterns
+    )
+)]
+
+#[macro_use]
+extern crate syn;
+
+extern crate proc_macro2;
+extern crate quote;
+
+#[path = "src/mod.rs"]
+mod internals;
+
+pub use internals::*;
