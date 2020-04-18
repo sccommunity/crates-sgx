8778451d5750e58d30881b6a99b0c751bed16f66
sammyne <lixiangmin01@baidu.com>
2020-04-18 07:27:38 +0000

    feature(color_quant): port to teaclave-sgx-sdk@v1.1.1

--- crates/color_quant/src/lib.rs
+++ crates/color_quant/src/lib.rs
@@ -60,6 +60,17 @@ that this copyright notice remain intact.
 //! let color_map = nq.color_map_rgba();
 //! ```
 //!
+
+#![cfg_attr(all(feature = "mesalock_sgx", not(target_env = "sgx")), no_std)]
+#![cfg_attr(
+    all(target_env = "sgx", target_vendor = "mesalock"),
+    feature(rustc_private)
+)]
+#[cfg(all(feature = "mesalock_sgx", not(target_env = "sgx")))]
+#[macro_use]
+extern crate sgx_tstd as std;
+use std::prelude::v1::*;
+
 use std::cmp::{max, min};
 
 macro_rules! clamp(
