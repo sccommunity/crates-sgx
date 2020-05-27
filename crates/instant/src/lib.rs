db6e70912d6e2b3bad390572d2c836e1050ae690
Zha0Chan <42883733@qq.com>
2020-05-27 16:43:12 +0800

    init instant

---
+++ crates/instant/src/lib.rs
@@ -0,0 +1,24 @@
+#[cfg(all(any(target_arch = "wasm32", target_arch = "asmjs"), feature = "stdweb"))]
+#[macro_use]
+extern crate stdweb;
+
+#[cfg(not(any(target_arch = "wasm32", target_arch = "asmjs")))]
+pub use crate::native::Instant;
+
+#[cfg(any(target_arch = "wasm32", target_arch = "asmjs"))]
+pub use crate::wasm::Instant;
+
+#[cfg(not(any(target_arch = "wasm32", target_arch = "asmjs")))]
+#[cfg(feature = "now")]
+pub use crate::native::now;
+
+#[cfg(any(target_arch = "wasm32", target_arch = "asmjs"))]
+#[cfg(feature = "now")]
+pub use crate::wasm::now;
+
+pub use std::time::Duration;
+
+#[cfg(not(any(target_arch = "wasm32", target_arch = "asmjs")))]
+mod native;
+#[cfg(any(target_arch = "wasm32", target_arch = "asmjs"))]
+mod wasm;
