fbefaa8d4e5fdcd22d0f9bb433b4bb8d107fa94f
Zha0Chan <42883733@qq.com>
2020-07-13 10:48:53 +0800

    init num-iter

---
+++ crates/num-iter/ci/rustup.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# Use rustup to locally run the same suite of tests as .github/workflows/
+# (You should first install/update all of the versions below.)
+
+set -ex
+
+ci=$(dirname $0)
+for version in 1.8.0 1.15.0 1.20.0 1.26.0 1.31.0 stable beta nightly; do
+    rustup run "$version" "$ci/test_full.sh"
+done
