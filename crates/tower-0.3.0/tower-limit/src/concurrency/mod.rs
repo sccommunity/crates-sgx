5e21700a7cc740e097bc46cb7e575d7411616fec
zhaochan <zhaochan@baidu.com>
2020-08-14 10:43:01 +0800

    remove tower-0.3.0

--- crates/tower-0.3.0/tower-limit/src/concurrency/mod.rs
+++
@@ -1,8 +0,0 @@
-//! Limit the max number of requests being concurrently processed.
-
-pub mod future;
-mod layer;
-mod service;
-mod sync;
-
-pub use self::{layer::ConcurrencyLimitLayer, service::ConcurrencyLimit};
--- crates/tower-0.3.0/tower-limit/src/rate/mod.rs
+++
@@ -1,7 +0,0 @@
-//! Limit the rate at which requests are processed.
-
-mod layer;
-mod rate;
-mod service;
-
-pub use self::{layer::RateLimitLayer, rate::Rate, service::RateLimit};
